7e3f54b4cfa4926b093a76c7a646942cb7d9333c
pedroenrre <pedro_enrre@hotmail.com>
2021-08-20 20:10:04 -0300

    adds forms for appointment create

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
 import { Header } from '../../components/Header';
@@ -8,45 +8,99 @@ import { styles } from './styles';
 import { theme } from '../../global/styles/theme';
 import { ButtonIcon } from '../../components/ButtonIcon';
 import { CategorySelect } from '../../components/CategorySelect';
+import { SmallInput } from '../../components/SmallInput';
+import { TextArea } from '../../components/TextArea';
+import { Button } from '../../components/Button';
 
 export const AppointmentCreate: React.FC = () => {
   const [category, setCategory] = useState('');
 
   return (
-    <>
-      <Header
-        title="Agendar partida"
-      />
-      <Text style={[
-        styles.label,
-        {marginLeft: 24, marginTop: 36, marginBottom: 16}]}
-      
-      >
-        Categoria
-      </Text>
-      <CategorySelect 
-        hasCheckBox
-        categorySelected={category}
-        setCategory={setCategory}
-      />
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height' }
+      style={styles.container}
+    >
+      <ScrollView>
+        <Header
+          title="Agendar partida"
+        />
+        <Text style={[
+          styles.label,
+          {marginLeft: 24, marginTop: 36, marginBottom: 16}]}
+        >
+          Categoria
+        </Text>
+        <CategorySelect 
+          hasCheckBox
+          categorySelected={category}
+          setCategory={setCategory}
+        />
 
-      <View style={styles.form}>
-        <RectButton>
-          <View style={styles.select}>
-            <View style={styles.image}/>
-            <Text style={styles.label}>
-              Selecione um servidor
-            </Text>
-            <Feather 
-              name="chevron-right"
-              color={theme.colors.heading}
-              size={18}
-            />
+        <View style={styles.form}>
+          <RectButton>
+            <View style={styles.select}>
+              <View style={styles.image}/>
+              <Text style={styles.label}>
+                Selecione um servidor
+              </Text>
+              <Feather 
+                name="chevron-right"
+                color={theme.colors.heading}
+                size={18}
+              />
+            </View>
+          </RectButton>
+          
+          <View style={styles.field}>
+            <View>
+              <Text style={styles.label}>
+                Dia e mês
+              </Text>
+              <View style={styles.collumn}>
+                <SmallInput maxLength={2}/>
+                <Text style={styles.divider}>
+                  /
+                </Text>
+                <SmallInput maxLength={2}/>
+              </View>
+            </View>
+
+            <View>
+              <Text style={styles.label}>
+                Hora e minuto
+              </Text>
+              <View style={styles.collumn}>
+                <SmallInput maxLength={2}/>
+                <Text style={styles.divider}>
+                  :
+                </Text>
+                <SmallInput maxLength={2}/>
+              </View>
+            </View>
           </View>
-        </RectButton>
-      </View>
 
-      
-    </>
+          <View style={[styles.field, {marginBottom: 12}]}>
+            <Text style={styles.label}>
+              Descrição
+            </Text>
+
+            <Text style={styles.caracteresLimit}>
+              Max 100 caracteres
+            </Text>
+          </View>
+          
+          <TextArea
+            multiline
+            maxLength={100}
+            numberOfLines={5}
+            autoCorrect={false}
+          />
+
+          <View style={styles.footer}>
+            <Button title="Agendar" />
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
--- src/screens/AppointmentCreate/styles.ts
+++ src/screens/AppointmentCreate/styles.ts
@@ -1,9 +1,9 @@
 import { StyleSheet } from 'react-native';
-import { getBottomSpace } from 'react-native-iphone-x-helper';
 import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
   container: {
+    flex: 1,
   },
   label:{
     fontSize:18,
@@ -31,5 +31,30 @@ export const styles = StyleSheet.create({
     height: 68,
     backgroundColor: theme.colors.secondary50,
     borderRadius: 8,
+  },
+  field:{
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 30,
+  },
+  collumn:{
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  divider:{
+    marginRight: 4,
+    fontSize: 18,
+    fontFamily: theme.fonts.text500,
+    color: theme.colors.heading,
+  },
+  caracteresLimit:{
+    fontSize: 13,
+    fontFamily: theme.fonts.text400,
+    color: theme.colors.heading
+  },
+  footer:{
+    marginTop: 20,
+    marginBottom: 56,
   },
 });
